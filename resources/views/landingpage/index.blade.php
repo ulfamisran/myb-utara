c3902260fb6ec9f21ac82b6fce055afeed0979d1
ulfamisran <nurulfadhillah0101@gmail.com>
2022-12-28 12:38:02 +0800

    add landing page fitur

---
+++ resources/views/landingpage/index.blade.php
@@ -0,0 +1,228 @@
+<!DOCTYPE html>
+	<html lang="zxx" class="no-js">
+	<head>
+		<!-- Mobile Specific Meta -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!-- Favicon-->
+		<link rel="shortcut icon" href="img/fav.png">
+		<!-- Author Meta -->
+		<meta name="author" content="colorlib">
+		<!-- Meta Description -->
+		<meta name="description" content="">
+		<!-- Meta Keyword -->
+		<meta name="keywords" content="">
+		<!-- meta character set -->
+		<meta charset="UTF-8">
+		<!-- Site Title -->
+		<title>Administrasi Desa Gura</title>
+
+		<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet">
+			<!--
+			CSS
+			============================================= -->
+			<link rel="stylesheet" href="{{url('landingpage/css/linearicons.css')}}">
+			<link rel="stylesheet" href="{{url('landingpage/css/font-awesome.min.css')}}">
+			<link rel="stylesheet" href="{{url('landingpage/css/bootstrap.css')}}">
+			<link rel="stylesheet" href="{{url('landingpage/css/magnific-popup.css')}}">
+			<link rel="stylesheet" href="{{url('landingpage/css/jquery-ui.css')}}">
+			<link rel="stylesheet" href="{{url('landingpage/css/nice-select.css')}}">
+			<link rel="stylesheet" href="{{url('landingpage/css/animate.min.css')}}">
+			<link rel="stylesheet" href="{{url('landingpage/css/owl.carousel.css')}}">
+			<link rel="stylesheet" href="{{url('landingpage/css/main.css')}}">
+		</head>
+		<body>
+			<header id="header">
+				<div class="header-top">
+					<div class="container">
+			  		<div class="row align-items-center">
+			  			<div class="col-lg-6 col-sm-6 col-6 header-top-left">
+			  				<!-- <ul>
+			  					<li><a href="#">Visit Us</a></li>
+			  					<li><a href="#">Buy Tickets</a></li>
+			  				</ul> -->
+			  			</div>
+			  			<div class="col-lg-6 col-sm-6 col-6 header-top-right">
+							<div class="header-social">
+								<a href="#"><i class="fa fa-facebook"></i></a>
+								<a href="#"><i class="fa fa-twitter"></i></a>
+								<a href="#"><i class="fa fa-dribbble"></i></a>
+								<a href="#"><i class="fa fa-behance"></i></a>
+							</div>
+			  			</div>
+			  		</div>
+					</div>
+				</div>
+				<div class="container main-menu">
+					<div class="row align-items-center justify-content-between d-flex">
+                        <div id="logo">
+                            <!-- <a href="index.html"><img src="landingpage/logo.png"  height="50" alt="" title="" /></a> -->
+                            <b><a style="font-size:18px;" class="text-white" href="index.html"><img src="gambarlogo/logo_enrekang.png"  height="50" alt="" title="" /> Desa Buntu Mondong</a></b>
+                        </div>
+                        <nav id="nav-menu-container">
+                            <ul class="nav-menu">
+                            <li><a href="index.html">Home</a></li>
+                            <li><a href="about.html">Cek Surat</a></li>
+
+                            <li><a href="contact.html">Contact</a></li>
+                            </ul>
+				        </nav><!-- #nav-menu-container -->
+					</div>
+				</div>
+			</header><!-- #header -->
+
+			<!-- start banner Area -->
+			<section class="banner-area relative">
+				<div class="overlay overlay-bg"></div>
+				<div class="container">
+					<div class="row fullscreen align-items-center justify-content-between">
+						<div class="col-lg-6 col-md-6 banner-left">
+                        <br><br><br>
+							<h6 class="text-white">Sistem Permohonan Pembuatan Surat</h6>
+							<h1 class="text-white">PERSURATAN DESA BUNTU MONDONG</h1>
+							<p class="text-white">
+								Pengurusan Surat Desa Buntu Mondong Lebih Mudah dan Efisien.
+                            </p>
+                            <p class="text-white">
+								Pastikan No. Penduduk dan No. Kartu Keluarga Terdaftar pada Sistem.
+                            </p><br>
+							<a href="#" class="primary-btn text-uppercase">CEK SURAT SAYA</a>
+						</div>
+						<div class="col-lg-6 col-md-6 banner-right">
+                        <br><br><br><br><br>
+							<ul class="nav nav-tabs" id="myTab" role="tablist">
+							  <li class="nav-item">
+							    <a class="nav-link active" id="flight-tab" data-toggle="tab" href="#flight" role="tab" aria-controls="flight" aria-selected="true">Cek No.KK</a>
+							  </li>
+							  <li class="nav-item">
+							    <a class="nav-link" id="hotel-tab" data-toggle="tab" href="#hotel" role="tab" aria-controls="hotel" aria-selected="false">Cek NIK</a>
+							  </li>
+							  <!-- <li class="nav-item">
+							    <a class="nav-link" id="holiday-tab" data-toggle="tab" href="#holiday" role="tab" aria-controls="holiday" aria-selected="false">PERMOHONAN SURAT</a>
+							  </li> -->
+							</ul>
+							<div class="tab-content" id="myTabContent">
+							  <div class="tab-pane fade show active" id="flight" role="tabpanel" aria-labelledby="flight-tab">
+								<form class="form-wrap">
+									<input type="text" class="form-control" name="name" placeholder="Masukkan No Kartu Keluarga " onfocus="this.placeholder = ''" onblur="this.placeholder = 'From '">
+									<a href="#" class="primary-btn text-uppercase">Cek Kartu Keluarga</a>
+								</form>
+							  </div>
+							  <div class="tab-pane fade" id="hotel" role="tabpanel" aria-labelledby="hotel-tab">
+								<form class="form-wrap">
+									<input type="text" class="form-control" name="name" placeholder="Masukkan NIK " onfocus="this.placeholder = ''" onblur="this.placeholder = 'From '">
+									<a href="#" class="primary-btn text-uppercase">Cek NIK </a>
+								</form>
+							  </div>
+
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!-- End banner Area -->
+
+
+
+
+
+			<!-- start footer Area -->
+			<footer class="footer-area section-gap">
+				<div class="container">
+
+					<div class="row">
+						<div class="col-lg-3  col-md-6 col-sm-6">
+							<div class="single-footer-widget">
+								<h6>About Agency</h6>
+								<p>
+									The world has become so fast paced that people don’t want to stand by reading a page of information, they would much rather look at a presentation and understand the message. It has come to a point
+								</p>
+							</div>
+						</div>
+						<div class="col-lg-3 col-md-6 col-sm-6">
+							<div class="single-footer-widget">
+								<h6>Navigation Links</h6>
+								<div class="row">
+									<div class="col">
+										<ul>
+											<li><a href="#">Home</a></li>
+											<li><a href="#">Feature</a></li>
+											<li><a href="#">Services</a></li>
+											<li><a href="#">Portfolio</a></li>
+										</ul>
+									</div>
+									<div class="col">
+										<ul>
+											<li><a href="#">Team</a></li>
+											<li><a href="#">Pricing</a></li>
+											<li><a href="#">Blog</a></li>
+											<li><a href="#">Contact</a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-3  col-md-6 col-sm-6">
+							<div class="single-footer-widget">
+								<h6>Newsletter</h6>
+								<p>
+									For business professionals caught between high OEM price and mediocre print and graphic output.
+								</p>
+								<div id="mc_embed_signup">
+									<form target="_blank" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get" class="subscription relative">
+										<div class="input-group d-flex flex-row">
+											<input name="EMAIL" placeholder="Email Address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email Address '" required="" type="email">
+											<button class="btn bb-btn"><span class="lnr lnr-location"></span></button>
+										</div>
+										<div class="mt-10 info"></div>
+									</form>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-3  col-md-6 col-sm-6">
+							<div class="single-footer-widget mail-chimp">
+								<h6 class="mb-20">InstaFeed</h6>
+								<ul class="instafeed d-flex flex-wrap">
+									<li><img src="img/i1.jpg" alt=""></li>
+									<li><img src="img/i2.jpg" alt=""></li>
+									<li><img src="img/i3.jpg" alt=""></li>
+									<li><img src="img/i4.jpg" alt=""></li>
+									<li><img src="img/i5.jpg" alt=""></li>
+									<li><img src="img/i6.jpg" alt=""></li>
+									<li><img src="img/i7.jpg" alt=""></li>
+									<li><img src="img/i8.jpg" alt=""></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+
+					<div class="row footer-bottom d-flex justify-content-between align-items-center">
+						<p class="col-lg-8 col-sm-12 footer-text m-0"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+						<div class="col-lg-4 col-sm-12 footer-social">
+							<a href="#"><i class="fa fa-facebook"></i></a>
+							<a href="#"><i class="fa fa-twitter"></i></a>
+							<a href="#"><i class="fa fa-dribbble"></i></a>
+							<a href="#"><i class="fa fa-behance"></i></a>
+						</div>
+					</div>
+				</div>
+			</footer>
+			<!-- End footer Area -->
+
+			<script src="{{url('landingpage/js/vendor/jquery-2.2.4.min.js')}}"></script>
+			<script src="{{url('landingpage/js/popper.min.js')}}"></script>
+			<script src="{{url('landingpage/js/vendor/bootstrap.min.js')}}"></script>
+			<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
+ 			<script src="{{url('landingpage/js/jquery-ui.js')}}"></script>
+  			<script src="{{url('landingpage/js/easing.min.js')}}"></script>
+			<script src="{{url('landingpage/js/hoverIntent.js')}}"></script>
+			<script src="{{url('landingpage/js/superfish.min.js')}}"></script>
+			<script src="{{url('landingpage/js/jquery.ajaxchimp.min.js')}}"></script>
+			<script src="{{url('landingpage/js/jquery.magnific-popup.min.js')}}"></script>
+			<script src="{{url('landingpage/js/jquery.nice-select.min.js')}}"></script>
+			<script src="{{url('landingpage/js/owl.carousel.min.js')}}"></script>
+			<script src="{{url('landingpage/js/mail-script.js')}}"></script>
+			<script src="{{url('landingpage/js/main.js')}}"></script>
+		</body>
+	</html>
--- routes/web.php
+++ routes/web.php
@@ -38,7 +38,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('landingpage.index');
 });
 
 //VIEW
